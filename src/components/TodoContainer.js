6b3b27fc4b3cdca14f744d4d72eb563d24af4076
Diego Doná <diedona@gmail.com>
2020-09-26 11:17:53 -0300

    deletando arquivos padrões e iniciando TodoApp

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+    
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>Estou aqui!</p>
+            </div>
+        );
+    }
+
+}
+
+export default TodoContainer;
