ed6afae19e83ab8f48d1a4b967ae9c1357031cce
Diego Doná <diedona@gmail.com>
2020-09-27 00:29:23 -0300

    Updates

---
+++ precache-manifest.02befee105f5d7b986c1266dc12b3073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80b9985fd68d2b95577793cc4edd2bf",
+    "url": "/diedona/react-todo-app.git/index.html"
+  },
+  {
+    "revision": "a2ac50ca8f427e7a1497",
+    "url": "/diedona/react-todo-app.git/static/css/main.f1580b57.chunk.css"
+  },
+  {
+    "revision": "68bafe6359bd2ccbe0e4",
+    "url": "/diedona/react-todo-app.git/static/js/2.8e41f98d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diedona/react-todo-app.git/static/js/2.8e41f98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ac50ca8f427e7a1497",
+    "url": "/diedona/react-todo-app.git/static/js/main.68d93505.chunk.js"
+  },
+  {
+    "revision": "9180e01671a725c1639e",
+    "url": "/diedona/react-todo-app.git/static/js/runtime-main.70240b1f.js"
+  }
+]);
